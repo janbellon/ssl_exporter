ed747540cd347396de91e4eb6197fb5f3e2cb60b
Jan Bellon <jan@nobell.fr>
2026-06-13 16:05:19 +0200

    Change environment variables gathering

--- internal/config/config.go
+++ internal/config/config.go
@@ -2,6 +2,7 @@ package config
 
 import (
 	"fmt"
+	"os"
 	"strings"
 
 	"github.com/spf13/viper"
@@ -25,33 +26,29 @@ type ClientConfig struct {
 
 func Load() (*Config, error) {
 	v := viper.New()
+
 	v.SetDefault("log_level", "info")
 	v.SetDefault("listen.host", "127.0.0.1")
 	v.SetDefault("listen.port", 9123)
 	v.SetDefault("listen.bearer", "")
 	v.SetDefault("client.timeout", 30)
 
-	configPath := viper.GetString("SSL_EXPORTER_CONFIG")
+	v.SetEnvPrefix("SSL_EXPORTER")
+	v.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+	v.AutomaticEnv()
+
+	configPath := os.Getenv("SSL_EXPORTER_CONFIG")
 	if configPath != "" {
 		v.SetConfigFile(configPath)
 
 		if err := v.ReadInConfig(); err != nil {
-			return nil, fmt.Errorf("Could not read config file, %w", err)
+			return nil, fmt.Errorf("could not read config file: %w", err)
 		}
 	}
 
-	v.SetEnvPrefix("SSL_EXPORTER")
-
-	replacer := strings.NewReplacer(".", "_")
-
-	v.SetEnvKeyReplacer(replacer)
-
-	v.AutomaticEnv()
-
 	var cfg Config
-
 	if err := v.Unmarshal(&cfg); err != nil {
-		return nil, fmt.Errorf("unable to decode configuration, %w", err)
+		return nil, fmt.Errorf("unable to decode configuration: %w", err)
 	}
 
 	return &cfg, nil
